3ce626a15790632d30ae95431b05cf73ccdc5b45
williamning <61674789+williamning@users.noreply.github.com>
2020-12-09 17:56:17 +0800

    init ...

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/hello world/i); // 要跟随组件一起改变
   expect(linkElement).toBeInTheDocument();
 });
